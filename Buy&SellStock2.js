5e69e20c5e844a9a2e2762e259090bd298f94ea4
DineshDhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-11-05 08:04:00 +0530

    Add files via upload

---
+++ Buy&SellStock2.js
@@ -0,0 +1,10 @@
+function profit(prices){
+    let prices = 0
+
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] > prices[i-1]){
+            profit += prices[i] - prices[i-1]
+        }
+    }
+    return profit
+}
