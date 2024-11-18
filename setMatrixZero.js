9b2c00f54530f7666d9e563df207148282beebce
Dinesh Dhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-11-18 20:14:30 +0530

    Add files via upload

--- setMatrixZero.js
+++ setMatrixZero.js
@@ -10,4 +10,35 @@ function setZeros(matrix) {
       break;
     }
   }
+  for (let i = 0; i < row; i++) {
+    if (matrix[i][0] === 0) {
+      firstcolzero = true;
+      break;
+    }
+  }
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < col; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < col; j++) {
+      if (matrix[i][0] === 0 || matrix[j][0] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  if (firstrowzero) {
+    for (let j = 0; j < col; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+  if (firstcolzero) {
+    for (let i = 0; i < row; i++) {
+      matrix[i][0] = 0;
+    }
+  }
 }
