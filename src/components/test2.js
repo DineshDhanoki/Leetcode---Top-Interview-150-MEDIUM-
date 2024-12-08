c1551dc2811598885cd804ed3a5b7a70b0f163d4
Dinesh Dhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-12-08 23:07:43 +0530

    Add files via upload

---
+++ src/components/test2.js
@@ -0,0 +1,5 @@
+const numbers = [0, 10, 20, 30, 40, 50];
+const number = numbers.map((nums) => nums / 2);
+const filtered = numbers.filter((num) => num <= 30);
+console.log(number);
+console.log(filtered);
